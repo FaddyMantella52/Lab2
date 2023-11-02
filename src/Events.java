8bad38e3e7af7729593ffa6262cd9105116e882e
Fadd52 <lucas11091109@gmail.com>
2023-11-02 20:54:17 +0200

    Lab2

---
+++ src/Events.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Events {
+    public String name;
+
+    private List<Slot> slots = new ArrayList<>();
+
+    public Events(String name){
+        this.name = name;
+    }
+
+    public void addSlot(Slot slot){
+        slots.add(slot);
+    }
+}
