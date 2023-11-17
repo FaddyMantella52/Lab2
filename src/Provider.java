ea5c87d6891faa61b7feac63e72124567720efd5
Fadd52 <lucas11091109@gmail.com>
2023-11-17 13:22:19 +0200

    Lab2

--- src/Provider.java
+++ src/Provider.java
@@ -1,9 +1,25 @@
-public class Provider {
+import java.util.ArrayList;
+import java.util.List;
+
+interface ProviderObservable {
+    void addObserver(ProviderObserver observer);
+    void removeObserver(ProviderObserver observer);
+    void notifyObservers(String message);
+}
+
+interface ProviderObserver {
+    void update(String message);
+}
+
+
+public class Provider implements ProviderObservable{
     public void setName(String name) {
         this.name = name;
     }
     private String name;
     private int ProviderID;
+    private List<Slot> slots = new ArrayList<>();
+    private List<ProviderObserver> observers = new ArrayList<>();
 
     @Override
     public String toString() {
@@ -31,5 +47,25 @@ public class Provider {
 
     public void createSlot(Slot slot){
         slot.addProvider(this);
+        notifyObservers("New slot created by provider " + name);
+    }
+
+    @Override
+    public void addObserver(ProviderObserver observer) {
+        observers.add(observer);
+
+    }
+
+    @Override
+    public void removeObserver(ProviderObserver observer) {
+        observers.remove(observer);
+
+    }
+
+    @Override
+    public void notifyObservers(String message) {
+        for (ProviderObserver observer : observers) {
+            observer.update(message);
+        }
     }
 }
